f8352d390bb2f6fdfbefd7833dd6a4bcb7513593
spakanati <spakanati@gmail.com>
2022-09-12 01:47:09 -0700

    Add a log

--- functions/ffprobe-installer.ts
+++ functions/ffprobe-installer.ts
@@ -4,6 +4,7 @@ const ffprobePath = require('@ffprobe-installer/ffprobe').path
 import { Request, Response } from 'express'
 
 export default async (req: Request, res: Response) => {
+  console.log('Starting ffprobe-installer test...')
   const child = exec([ffprobePath, '-version'].join(' '))
   child.stdout.on('data', function (data) {
     console.log('stdout: ' + data)
