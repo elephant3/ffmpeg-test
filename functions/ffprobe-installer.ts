0f56bd433fb211269d2e771b537acf87f72cdb39
spakanati <spakanati@gmail.com>
2022-09-12 01:43:57 -0700

    first commit

---
+++ functions/ffprobe-installer.ts
@@ -0,0 +1,24 @@
+import { exec } from 'child_process'
+const ffprobePath = require('@ffprobe-installer/ffprobe').path
+
+import { Request, Response } from 'express'
+
+export default async (req: Request, res: Response) => {
+  const child = exec([ffprobePath, '-version'].join(' '))
+  child.stdout.on('data', function (data) {
+    console.log('stdout: ' + data)
+  })
+  child.stderr.on('data', function (data) {
+    console.log('stderr: ' + data)
+  })
+  child.on('close', function (code) {
+    console.log('closing code: ' + code)
+    if (code === 0) {
+      res.status(200).send('Success!')
+    } else {
+      res.status(500).send('Failed!')
+    }   
+  })
+
+  
+}
