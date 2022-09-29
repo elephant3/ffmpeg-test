f648dcc54edecc82a60b705d1c78527bd6f0f126
spakanati <spakanati@gmail.com>
2022-09-28 18:23:14 -0700

    test

--- functions/ffmpeg-static.ts
+++ functions/ffmpeg-static.ts
@@ -5,23 +5,36 @@ const ffmpegPath  = require('ffmpeg-static')
 
 export default async (req: Request, res: Response) => {
   console.log('Starting ffmpegStatic -static test...')
+  require('child_process').exec(
+    'cp /var/task/ffmpeg /tmp/.; chmod 755 /tmp/ffmpeg;',
+    function (error, stdout, stderr) {
+      if (error) {
+        //handle error
+      } else {
+        console.log("stdout: " + stdout)
+        console.log("stderr: " + stderr)
+        //handle success
+        console.log(`PATH: ${ffmpegPath}`);
+        const child = exec(`tmp/ffmpeg -version`)
+        child.stdout.on('data', function (data) {
+          console.log('stdout: ' + data)
+        })
+        child.stderr.on('data', function (data) {
+          console.log('stderr: ' + data)
+        })
+        child.on('close', function (code) {
+          console.log('closing code: ' + code)
+          if (code === 0) {
+            res.status(200).send('Success!')
+          } else {
+            res.status(500).send('Failed!')
+          }   
+        })
+      
+      }
+    }
+  )
   
-  console.log(`PATH: ${ffmpegPath}`);
-  const child = exec([ffmpegPath, '-version'].join(' '))
-  child.stdout.on('data', function (data) {
-    console.log('stdout: ' + data)
-  })
-  child.stderr.on('data', function (data) {
-    console.log('stderr: ' + data)
-  })
-  child.on('close', function (code) {
-    console.log('closing code: ' + code)
-    if (code === 0) {
-      res.status(200).send('Success!')
-    } else {
-      res.status(500).send('Failed!')
-    }   
-  })
 
   
 }
