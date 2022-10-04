7126b0a4fa99eb29363086b1918cf389b612c1ea
Siarhei Lipchyk <contact@siarhei.dev>
2022-10-04 18:43:29 +0200

    wip

--- functions/ffmpeg-installer.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg'
-import { runCommand } from './_command'
-
-export default async (_: Request, res: Response) => {  
-  runCommand(ffmpegPath, res)
-}
--- functions/ffprobe-static.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-import { path as ffprobePath } from 'ffprobe-static'
-import { runCommand } from './_command'
-
-export default async (_: Request, res: Response) => {
-  runCommand(ffprobePath, res)
-}
